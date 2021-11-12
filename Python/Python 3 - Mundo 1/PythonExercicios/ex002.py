9e47eba38514c8fe3c888ac555f3b09eec0ed3b6
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-12 10:53:18 -0400

    Exercicios 002 e 003

--- Python/Python 3 - Mundo 1/PythonExercicios/ex002.py
+++ Python/Python 3 - Mundo 1/PythonExercicios/ex002.py
@@ -1,3 +1,13 @@
+#Desafio 01
+nome = input('Qaul é o seu nome?')
+print('Olá {} !, prazer em te conhecer!'.format(nome))
+
+#Desafio 02
+dia = input('Informe o dia do seu nascimento?')
+mes = input('Informe o mes do seu nasciento?')
+ano = input('Informe o ano do seu nascimento?')
+print('Você nasceu no dia {}/{}/{}. Correto?'.format(dia, mes, ano))
+
 # Exercício Python 2: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 nome = input('Digite seu nome:')
 print('É um prazer te conhecer, {}!'.format(nome))
