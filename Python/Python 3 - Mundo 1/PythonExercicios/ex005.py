073d97b537f14f25e14ef6e00bf2ce8c7eb66f9c
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-12 15:59:46 -0400

    Exercicio 005

---
+++ Python/Python 3 - Mundo 1/PythonExercicios/ex005.py
@@ -0,0 +1,4 @@
+# Exercício Python 5: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+numero = int(input('Informe um número inteiro: '))
+print('O seu numério é {}, o seu sucessor é {}, e o seu antecessor é {}'.format(numero, numero + 1, numero -1))
