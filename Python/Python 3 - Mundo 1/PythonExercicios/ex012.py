6b811f63c11b839e8036a52bb4f2fad6394da9ea
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-12 16:57:28 -0400

    Exercício 012

---
+++ Python/Python 3 - Mundo 1/PythonExercicios/ex012.py
@@ -0,0 +1,3 @@
+# Exercício Python 12: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+preco = float(input('Qual é o preço do produto? R$ '))
+print('O produto que custava R${:.2f} com desconto de 5% vai custar R$ {:.2f}'.format(preco, (preco*0.95)))
