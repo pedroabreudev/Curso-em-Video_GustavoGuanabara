858a66ed4909eb611513f36c61f5337411335440
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-12 16:00:54 -0400

    Aula 007

---
+++ Python/Python 3 - Mundo 1/PythonExercicios/Aulas/aula07a.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+soma = n1 + n2
+mutiplicacao = n1 * n2
+divisao = n1 / n2
+divisao_inteira = n1 // n2
+potencia = n1 ** n2
+
+print('A soma é {}\nO Produto é {}\nA divisão é {}'.format(soma, mutiplicacao, divisao))
+print('A Divisão inteira é {}\nA potência é {}'.format(divisao_inteira, potencia))
+
