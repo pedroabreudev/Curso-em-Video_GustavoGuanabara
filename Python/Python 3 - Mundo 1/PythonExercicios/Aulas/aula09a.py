a181e2a797922815c37a8027685e2d76a33eb6cd
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-13 16:54:40 -0400

    Aula 009

---
+++ Python/Python 3 - Mundo 1/PythonExercicios/Aulas/aula09a.py
@@ -0,0 +1,6 @@
+#Manipulando cadeias de texto
+frase = 'Curso em Vídeo Python'
+print(frase[:13])
+print(frase.split())
+divido = frase.split()
+print('O tamanho da frase é {} caracteres'.format(len(frase)))
