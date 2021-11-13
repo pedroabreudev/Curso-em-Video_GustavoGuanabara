668e78e865c5845502a6f5ed6d23e59cf4212771
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-13 10:29:24 -0400

    Aula 008

---
+++ Python/Python 3 - Mundo 1/PythonExercicios/Aulas/aula08a.py
@@ -0,0 +1,10 @@
+# Trabalhando com modulos
+import random
+from math import sqrt
+
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print('A raiz de {} é igual a {:.2f}'.format(num, raiz))
+
+num1 = random.random()
+print(num1)
