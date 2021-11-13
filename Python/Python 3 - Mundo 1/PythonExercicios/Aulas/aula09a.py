9dbc9b288bc0bee1e44966b0d4e835b7265cd9de
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-13 16:57:01 -0400

    Aula 009

--- Python/Python 3 - Mundo 1/PythonExercicios/Aulas/aula09a.py
+++ Python/Python 3 - Mundo 1/PythonExercicios/Aulas/aula09a.py
@@ -4,3 +4,8 @@ print(frase[:13])
 print(frase.split())
 divido = frase.split()
 print('O tamanho da frase é {} caracteres'.format(len(frase)))
+
+#Nessa aula, vamos aprender operações com String no Python.
+# As principais operações que vamos aprender são o Fatiamento de String,
+# Análise com len(), count(), find(), transformações com replace(), upper(),
+# lower(), capitalize(), title(), strip(), junção com join().
