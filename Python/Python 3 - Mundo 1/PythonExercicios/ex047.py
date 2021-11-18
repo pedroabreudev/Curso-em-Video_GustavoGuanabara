646b93f28239566dd7b184026ca7c3c06cd18c6c
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-18 15:53:33 -0400

    Exercício 047

---
+++ Python/Python 3 - Mundo 1/PythonExercicios/ex047.py
@@ -0,0 +1,5 @@
+# Exercício Python 47: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+for numero in range(2, 51, 2):
+    if (numero % 2) == 0:
+        print(numero, end=' ')
+print('Acabou!')
