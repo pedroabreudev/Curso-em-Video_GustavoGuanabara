c1214c06808af78a25032133b5b35eda109831f6
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-19 16:51:29 -0400

    Exercício 057

---
+++ Python/Python 3 - Mundo 1/PythonExercicios/ex057.py
@@ -0,0 +1,7 @@
+# Exercício Python 57: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’.
+# Caso esteja errado, peça a digitação novamente até ter um valor correto.
+
+sexo = str(input('Informe seu sexo [M/F]: ')).upper()[0].strip()
+while sexo not in 'MF':
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso!'.format(sexo))
