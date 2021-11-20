6650d3860c298d54b2086f0d16745472acfde754
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-20 18:07:04 -0400

    Execício 065

---
+++ Python/Python 3 - Mundo 1/PythonExercicios/ex065.py
@@ -0,0 +1,20 @@
+# Exercício Python 65: Crie um programa que leia vários números inteiros pelo teclado.
+# No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos.
+# O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+resp = 'S'
+soma = quant = média = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        elif num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+media = soma / quant
+print('Você digitou {} números, o maior é {}, o menor é {} e a média foi {}'.format(quant, maior, menor, media))
+print('Acabou!')
